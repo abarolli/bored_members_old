8f7649c4667451b535e0839e3bdc6a78197cba82
Oni Barolli <obarolli@gmail.com>
2021-07-25 23:24:45 -0700

    Built final landing sections

---
+++ static/scripts/main.js
@@ -0,0 +1,27 @@
+function animateOnIntersection(entry, animationName) {
+  const target = entry.target;
+  console.log(target);
+  if (entry.isIntersecting) {
+      target.classList.add(animationName);
+      return;
+  }
+  target.classList.remove(animationName);
+}
+
+const pinkObserver = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    animateOnIntersection(entry, "slide-left-animate");
+  });
+});
+
+const purpleObserver = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    animateOnIntersection(entry, "slide-right-animate");
+  })
+})
+
+let mrPinks = document.querySelectorAll(".mr-pink");
+let mrPurple = document.querySelector(".final-landing .mr-purple");
+
+mrPinks.forEach(pink => pinkObserver.observe(pink));
+purpleObserver.observe(mrPurple);
