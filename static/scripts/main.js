4553ae17c3109cfd5ed611073126350351546b2e
Oni Barolli <obarolli@gmail.com>
2021-07-28 14:49:03 -0700

    Updated some stuff

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,33 +1,3 @@
-function animateOnIntersection(entry, animationName) {
-  const target = entry.target;
-  console.log(target);
-  if (entry.isIntersecting) {
-      target.classList.add(animationName);
-      return;
-  }
-  target.classList.remove(animationName);
-}
-
-const pinkObserver = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    animateOnIntersection(entry, "slide-left-animate");
-  });
-});
-
-const purpleObserver = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    animateOnIntersection(entry, "slide-right-animate");
-  })
-})
-
-let mrPinks = document.querySelectorAll(".mr-pink");
-let mrPurple = document.querySelector(".final-landing .mr-purple");
-
-mrPinks.forEach(pink => pinkObserver.observe(pink));
-purpleObserver.observe(mrPurple);
-
-
-
 // NAV MENU ANIMATIONS
 let listItems = document.querySelectorAll(".list__item");
 
--- static/scripts/prevent_transition.js
+++ static/scripts/prevent_transition.js
@@ -2,4 +2,5 @@ const body = document.body;
 
 body.onload = e => {
     body.classList.remove("preload");
+    AOS.init();
 }
