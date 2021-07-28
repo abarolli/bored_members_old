50cefcfaf194d5c8a7e29a75f3a2b5a87305b4d7
Oni Barolli <obarolli@gmail.com>
2021-07-28 14:22:34 -0700

    Added dropdown menu to navbar

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -25,3 +25,46 @@ let mrPurple = document.querySelector(".final-landing .mr-purple");
 
 mrPinks.forEach(pink => pinkObserver.observe(pink));
 purpleObserver.observe(mrPurple);
+
+
+
+// NAV MENU ANIMATIONS
+let listItems = document.querySelectorAll(".list__item");
+
+function showElement(element) {
+    element.style.visibility = "visible";
+    
+    let classList = element.classList;
+    if (classList.contains("hidden")) {
+        classList.replace("hidden", "visible");
+    }
+    else {
+        classList.add("visible");
+    }
+}
+
+function hideElement(element) {
+    let elementClassList = element.classList;
+    if (elementClassList.contains("visible")) {
+        elementClassList.replace("visible", "hidden");
+    }
+    else {
+        elementClassList.add("hidden");
+    }
+    setTimeout(() => {
+        element.style.visibility = "hidden";
+    }, 150);
+}
+
+
+listItems.forEach(item => {
+    item.onmouseover = e => {
+        let dropDown = item.querySelector(".drop-down");
+        showElement(dropDown);
+    };
+
+    item.onmouseleave = e => {
+        let dropDown = item.querySelector(".drop-down");
+        hideElement(dropDown);
+    };
+});
